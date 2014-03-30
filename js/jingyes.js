9bbb9ee8ccdce817c5a6bc1b223320c8a53fa6dc
achun <achun.shx@qq.com>
2014-03-31 02:11:37 +0800

    coding

--- js/jingyes.js
+++ js/jingyes.js
@@ -1,299 +1,405 @@
-! function(window, $) {
-  "use strict";
-  var jy = window.JY = window.JY || {};
-  var delay = 5000;
-  var win = $(window);
-  var body = $(body);
-  var onWinScroll = []; // {callback:function(...){},[self,skip]}
-  win.on('scroll.jingyes', function() {
-    for (var i = onWinScroll.length - 1; i >= 0; i--) {
-      var that = onWinScroll[i];
-      if (that && that.callback && !that.skip) {
-        that.callback.apply(that, arguments);
-      }
-    };
-  });
+! function(window,$) {
+	"use strict";
+	var document = window.document;
+	var jy = window.JY = window.JY || {};
+	function clearTextNode(jq, option) {
+		if (!document.body.classList) return false;
+		if (!jq) jq = [document.body];
 
-  $.support = $.support || {};
-  if (!$.support.transition) {
-    $.support.transition = (function() {
-      var el = document.firstChild,
-        transEndEventNames = {
-          'WebkitTransition': 'webkitTransitionEnd',
-          'MozTransition': 'transitionend',
-          'OTransition': 'oTransitionEnd otransitionend',
-          'transition': 'transitionend'
-        };
-      while (el && !el.style) el = el.nextSibling;
-      if (el)
-        for (name in transEndEventNames) {
-          if (el.style[name] !== undefined) {
-            return {
-              end: transEndEventNames[name]
-            };
-          }
-        }
-      return {};
-    })();
-  }
+		option = option || {};
+		option.deep = option.deep || 1;
+		var stop = false,
+			S = /\S/,
+			t = (new Date()).getTime(),
+			skipNodeNames = ['STYLE', 'SCRIPT', 'P', 'PRE', 'CODE'].concat(option.skipNodeNames),
+			skipClass = option.skipClass || "ctnskip",
+			stopClass = option.stopClass || "ctnskip",
+			node, deep;
+		for (var i = jq.length - 1; i >= 0; i--) {
+			node = jq[i];
+			if (node === document) {
+				node = document.body;
+			}
+			if (!node || !node.normalize) continue;
+			node.normalize();
+			deep = option.deep;
+			stop = false;
+			walk(node.firstChild);
+			node.normalize();
+		};
+		return ((new Date()).getTime() - t) / 1000;
 
-  function log() {
-    if (!window.console || !window.console.log) {} else {
-      window.console.log.apply(window.console, arguments);
-    }
-  }
+		function skip(node) {
+			if (skipNodeNames.indexOf(node.nodeName) !== -1) return true;
+			if (node.classList.contains(stopClass)) {
+				return stop = true;
+			}
+			return node.classList.contains(skipClass);
+		}
 
-  function dj(jq, ns, defaultKey) {
-    jq = $(jq);
-    var dat = jq.data(ns || "jy") || $(jq).data("jy") || "";
-    if (!dat) return;
+		function walk(node) {
+			while (!stop && node) {
+				if (node.nodeType === 3 && node.nextSibling && node.previousSibling && !S.test(node.data)) {
+					node.data = node.data.trim();
+				}
+				if (node.nodeType === 1 && !skip(node) && deep != 0) {
+					deep--;
+					walk(node.firstChild);
+					deep++;
+				}
+				node = node.nextSibling;
+			}
+		}
+	};
 
-    if (dat[0] == "{" || dat[0] == "[") {
-      dat = Function("return " + dat);
-      if (!$.isArray(dat)) {
-        return dat[ns || ""] || dat;
-      }
-    }
-    if (!defaultKey) return dat;
+	jy.clearTextNode = jy.clearTextNode || clearTextNode;
+	jy.init = jy.init || JingYesInit;
+	if (window && $){
+		jy.init(window,$);
+	}
 
-    var obj = {};
-    obj[defaultKey] = dat;
-    return obj;
-  }
+	function log() {
+		if (!arguments.length || !window.console || !window.console.log) {} else {
+			window.console.log.apply(window.console, arguments);
+		}
+	}
+	jy.log = jy.log || log;
+	var inited = false
+	function JingYesInit(window, $) {
+		if (inited) return;
+		inited = true;
+		var document = window.document;
+		var jy = window.JY = window.JY || {};
+		var intervals = 5000; // for setTimeout
+		var win = $(window);
+		var body = $(body);
+		var onWinScroll = []; // {callback:function(...){},[self,skip]}
+		var events = {
+			"*": true
+		};
 
-  function lastSplit(str, separator) {
-    var idx = str.lastIndexOf(separator);
-    if (idx == -1) return [];
-    return [str.substr(0, idx), str.substr(idx + 1)];
-  }
-  jy.lastSplit = lastSplit;
+		jy.intervals = function(milliseconds) {
+			intervals = parseInt(milliseconds) || 5000;
+		}
 
-  jy.convertImageToCanvas = function(image) {
-    var canvas = document.createElement("canvas");
-    canvas.width = image.width;
-    canvas.height = image.height;
-    canvas.getContext("2d").drawImage(image, 0, 0);
+		on(win, 'scroll.jingyes', function() {
+			for (var i = onWinScroll.length - 1; i >= 0; i--) {
+				var that = onWinScroll[i];
+				if (that && that.callback && !that.skip) {
+					that.callback.apply(that, arguments);
+				}
+			};
+		});
 
-    return canvas;
-  }
+		$.support = $.support || {};
+		if (!$.support.transition) {
+			$.support.transition = (function() {
+				var el = document.firstChild,
+					transEndEventNames = {
+						'WebkitTransition': 'webkitTransitionEnd',
+						'MozTransition': 'transitionend',
+						'OTransition': 'oTransitionEnd otransitionend',
+						'transition': 'transitionend'
+					};
+				while (el && !el.style) el = el.nextSibling;
+				if (el)
+					for (name in transEndEventNames) {
+						if (el.style[name] !== undefined) {
+							return {
+								end: transEndEventNames[name]
+							};
+						}
+					}
+				return {};
+			})();
+		}
 
-  jy.clearTextNode = function(node, option) {
-    if (!document.body.classList) return false;
-    if (!node) node = document;
-    if (!node.firstChild) return false;
+		function on(jq, name, fn) {
+			jq.on(name, function(e) {
+				if (events["*"] && events[name] === undefined || events[name])
+					fn.apply(this, arguments);
+			});
+		}
+		jy.on = on;
 
-    option = option || false;
-    var stop = false,
-      S = /\S/,
-      t = (new Date()).getTime()
-      //option
-      ,
-      skipNodeNames = ['STYLE', 'SCRIPT', 'P', 'PRE', 'CODE'].concat(option.skipNodeNames) //遇到标签跳过
-      ,
-      skipClass = option.skipClass || 'ctnskip' //遇到class跳过
-      ,
-      stopClass = option.stopClass || 'ctntop'; //遇到class终止
-    node.normalize();
-    walk(node);
-    node.normalize();
-    return ((new Date()).getTime() - t) / 1000;
+		function live(jq, name, fn) {
+			jq.live(name, function() {
+				if (events["*"] && events[name] === undefined || events[name])
+					fn.apply(this, arguments);
+			});
+		}
+		jy.live = live;
 
-    function skip(node) {
-      if (skipNodeNames.indexOf(node.nodeName) !== -1) return true;
-      if (node.classList.contains(stopClass)) {
-        return stop = true;
-      }
-      return node.classList.contains(skipClass);
-    }
+		jy.eventIsDisable = function(name) {
+			return events["*"] == false || events[name] == false;
+		}
 
-    function walk(node) {
-      node = node.firstChild;
-      while (!stop && node) {
-        if (node.nodeType === 3 && node.nextSibling && node.previousSibling && !S.test(node.data)) {
-          node.data = node.data.trim();
-        }
-        node.nodeType === 1 && !skip(node) && walk(node);
-        node = node.nextSibling;
-      }
-    }
-  }
+		jy.eventEnable = function(name) {
+			events[name || "*"] = true;
+		}
 
-  jy.accordion = function(nav_dl_dt) {
-    $(nav_dl_dt).on('click.jingyes.collapse', function() {
-      var dl = $(this.parentNode),
-        dd = $(this).next('dd'),
-        visible = dd.hasClass('open'),
-        dds = dd.siblings('dt+dd'),
-        fakeEnd = 'fake.transition.end',
-        eventEnd = $.support.transition.end || fakeEnd;
-      if (!dd.size() || !dl.hasClass('accordion') || dl.hasClass('keep') && visible) return;
-      if (visible) {
-        dd.removeClass('transition')
-          .each(scrollHeight)
-          .addClass('in transition')
-          .one(eventEnd, complete)
-          .height(0).trigger(fakeEnd)
-      } else {
-        dds.removeClass('transition')
-        dd.removeClass('transition')
+		jy.eventDisable = function(name) {
+			events[name || "*"] = false;
+		}
 
-        dds.each(scrollHeight)
-        dd.height(0)
+		function dj(jq, ns, defaultKey) {
+			jq = $(jq);
+			var dat = jq.data(ns || "jy") || $(jq).data("jy") || "";
+			if (!dat) return;
 
-        dds.one(eventEnd, complete)
-        dd.one(eventEnd, complete)
+			if (dat[0] == "{" || dat[0] == "[") {
+				dat = Function("return " + dat);
+				if (!$.isArray(dat)) {
+					return dat[ns || ""] || dat;
+				}
+			}
+			if (!defaultKey) return dat;
 
-        dds.addClass('in transition')
-        dd.addClass('open in transition')
+			var obj = {};
+			obj[defaultKey] = dat;
+			return obj;
+		}
 
-        dds.height(0).trigger(fakeEnd)
-        dd.each(scrollHeight).trigger(fakeEnd)
-      }
+		function lastSplit(str, separator) {
+			var idx = str.lastIndexOf(separator);
+			if (idx == -1) return [];
+			return [str.substr(0, idx), str.substr(idx + 1)];
+		}
+		jy.lastSplit = lastSplit;
 
-      function scrollHeight() {
-        $(this).height(this.scrollHeight)[0].offsetWidth;
-      }
+		jy.convertImageToCanvas = function(image) {
+			var canvas = document.createElement("canvas");
+			canvas.width = image.width;
+			canvas.height = image.height;
+			canvas.getContext("2d").drawImage(image, 0, 0);
 
-      function complete() {
-        var ths = $(this).removeClass('transition in');
-        !ths.height() && ths.removeClass('open');
-        ths.height('')[0].offsetWidth;
-        ths.addClass('transition');
-      }
-    }).next('dd').addClass('transition');
-  }
-  jy.accordion('nav>dl>dt');
+			return canvas;
+		}
 
+		jy.accordion = function(nav_dl_dt) {
+			on($(nav_dl_dt), 'click.jingyes.collapse', function() {
+				var dl = $(this.parentNode),
+					dd = $(this).next('dd'),
+					visible = dd.hasClass('open'),
+					dds = dd.siblings('dt+dd'),
+					fakeEnd = 'fake.transition.end',
+					eventEnd = $.support.transition.end || fakeEnd;
+				if (!dd.size() || !dl.hasClass('accordion') || dl.hasClass('keep') && visible) return;
+				if (visible) {
+					dd.removeClass('transition')
+						.each(scrollHeight)
+						.addClass('in transition')
+						.one(eventEnd, complete)
+						.height(0).trigger(fakeEnd)
+				} else {
+					dds.removeClass('transition')
+					dd.removeClass('transition')
 
-  //carousel
-  jy.carousel = function(dlCarousel) {
-    $(dlCarousel).each(function() {
-      var dts = $(this).children('dt');
-      if (dts.size() < 2) return;
-      var fakeEnd = 'fake.transition.end',
-        eventEnd = $.support.transition.end || fakeEnd;
-      var active = dts.eq(0);
-      var h = active.height(),
-        w = active.width();
-      var index = 0;
-      var stop = 0;
-      active.css({
-        display: 'inline-block',
-        width: w,
-        height: h
-      }).siblings().css({
-        display: 'inline-block',
-        width: 0,
-        height: h
-      })[0].offsetWidth;
-      dts.addClass('transition');
-      $(this).hover(
-        function() {
-          stop = 1;
-        },
-        function() {
-          if (stop == 2) setTimeout(run, delay);
-          stop = 0;
-        }
-      );
+					dds.each(scrollHeight)
+					dd.height(0)
 
-      function run() {
-        if (stop) {
-          stop = 2;
-          return;
-        }
-        active.height(h).width(0)[0].offsetWidth;
-        active = active.next();
-        if (!active.size())
-          active = dts.eq(0);
-        active.one(eventEnd, complete).height(h).width(w)[0].offsetWidth;
-      }
+					dds.one(eventEnd, complete)
+					dd.one(eventEnd, complete)
 
-      function complete() {
-        if (stop) stop = 2;
-        setTimeout(run, delay);
-      }
-      setTimeout(run, Math.random() * 6777);
-    });
-  }
-  jy.carousel('dl.carousel');
+					dds.addClass('in transition')
+					dd.addClass('open in transition')
 
-  //tabs
-  jy.tabs = function(nav_header_span) {
-    $(nav_header_span).click(function(e) {
-      $(this).addClass("active").siblings(this.nodeName).removeClass("active");
-      $(this.parentNode).siblings("div").children("div")
-        .removeClass("active").eq($(this).index()).addClass("active");
-      if ($(e.target).is("a")) {
-        return false;
-      }
-    });
-  }
-  jy.tabs('nav>header>span');
+					dds.height(0).trigger(fakeEnd)
+					dd.each(scrollHeight).trigger(fakeEnd)
+				}
 
-  //toggle class of elements by pattern
-  jy.toggle = function(jq, pattern) {
-    pattern = pattern || dj(jq, 'toggle');
-    pattern = pattern.split(',');
-    if (!pattern.length) return;
+				function scrollHeight() {
+					$(this).height(this.scrollHeight)[0].offsetWidth;
+				}
 
-    var t = [];
-    for (var i = pattern.length - 1; i >= 0; i--) {
-      var a = lastSplit(pattern[i], ".");
-      if (a.length == 2)
-        t.push(a);
-      else
-        log("invaild toggle rel: " + pattern[i]);
-    };
-    $(jq).click(function() {
-      for (var i = t.length - 1; i >= 0; i--) {
-        $(t[i][0]).toggleClass(t[i][1]);
-      }
-      return false;
-    });
-  }
-  jy.toggle('.toggle');
+				function complete() {
+					var ths = $(this).removeClass('transition in');
+					!ths.height() && ths.removeClass('open');
+					ths.height('')[0].offsetWidth;
+					ths.addClass('transition');
+				}
+			});
+			$(nav_dl_dt).next('dd').addClass('transition');
+		}
 
-  //affix-top
-  jy.affix = function(jq, opt) {
-    jq = $(jq).eq(0);
-    opt = typeof opt == "string" ? {
-      target: opt
-    } : opt || {};
-    var self = $.extend({
-      offset: 10,
-    }, dj(jq, "affix", 'target'), opt);
+		// carousel
+		// 参数 delayMilliseconds 用于延迟触发事件, 使动态效果错开运行.
+		jy.carousel = function(jq, delayMilliseconds) {
 
-    var target = self.target;
-    if (target) target = $(target);
-    // namespace
-    self.ns = "affix";
-    self.callback = function() {
-      var scrollTop = win.scrollTop();
-      if (jq.is('.affix-top')) {
-        var affixTop = 0,
-          top = jq.offset().top - scrollTop;
-        if (target) {
-          if (target.css('position') == 'fixed')
-            affixTop = target.position().top + target.outerHeight();
-        }
+			var jqs = $(jq);
+			var stop = false;
+			var timer = -1;
+			var carousel = function(delay) {
+				timer = setTimeout(function() {
+					jqs.trigger("carouse.jingyes");
+				}, delay);
+			};
 
-        if (self.offset + affixTop >= top) {
-          self.parentTop = jq.parent().offset().top - scrollTop;
-          jq.addClass("affix").removeClass('affix-top').css({
-            top: self.offset + affixTop
-          });
-        }
-      } else {
-        if (self.parentTop < jq.parent().offset().top - scrollTop) {
-          jq.addClass("affix-top").removeClass('affix');
-        }
-      }
-    }
+			jqs.hover(
+				function() {
+					clearTimeout(timer);
+					stop = true;
+				},
+				function() {
+					stop = false;
+					carousel(intervals);
+				}
+			);
 
-    onWinScroll.push(self);
-  }
-  $(function() {
-    jy.affix('.affix-top');
-  })
+			jy.clearTextNode(jqs, {
+				deep: 2
+			});
+			// 初始化检查
+			jqs.each(function() {
+				var p = $(this).parent();
+
+				var totalWidth = 0;
+				var cs = $(this).children('div');
+				cs.each(function() {
+					totalWidth += $(this).width();
+				}).eq(0).addClass("transition");
+
+				if (p.css('overflow') != "hidden") {
+					p.css({
+						overflow: 'hidden',
+						width: cs.eq(0).width()
+					});
+				}
+
+				$(this).width(totalWidth);
+			});
+
+			jqs.on("carouse.jingyes", function() {
+
+				if (!stop || !jy.eventIsDisable("carouse.jingyes")) {
+
+					jqs.each(function() {
+						var self = $(this);
+						var cs = self.children('div');
+						var first = cs.eq(0);
+
+						if (cs.length < 2) return;
+						var w = first.width();
+						var left = parseInt(first.css("marginLeft")) - w;
+						if (w * cs.length <= -left) {
+							left = 0;
+						}
+						first.css("marginLeft", left);
+					});
+				}
+
+				carousel(intervals);
+
+			});
+			carousel(delayMilliseconds || Math.random() * 7777);
+		}
+
+		//tabs
+		jy.tabs = function(nav_header_span) {
+			$(nav_header_span).click(function(e) {
+				$(this).addClass("active").siblings(this.nodeName).removeClass("active");
+				$(this.parentNode).siblings("div").children("div")
+					.removeClass("active").eq($(this).index()).addClass("active");
+				if ($(e.target).is("a")) {
+					return false;
+				}
+			});
+		}
+
+		//toggle class of elements by pattern
+		jy.toggle = function(jq, pattern) {
+			function target(jq,pattern){
+				var t = [];
+				pattern = pattern || dj(jq, 'toggle') || "";
+				if (!pattern) return t;
+				
+				pattern = pattern.split(',');
+				
+				for (var i = pattern.length - 1; i >= 0; i--) {
+					var a = lastSplit(pattern[i], ".");
+					if (a.length == 2)
+						t.push(a);
+					else
+						log("invaild toggle: " + pattern[i]);
+				};
+				return t;
+			}
+
+			var def = target(jq,pattern);
+
+			$(jq).click(function() {
+				var t = target(this);
+				if (!t.length) t= def;
+				for (var i = t.length - 1; i >= 0; i--) {
+					$(t[i][0]).toggleClass(t[i][1]);
+				}
+				return false;
+			});
+		}
+
+		//affix-top
+		jy.affix = function(jq, opt) {
+			jq = $(jq).eq(0);
+			if (!jq.length) return;
+
+			opt = typeof opt == "string" ? {
+				target: opt
+			} : opt || {};
+
+			var self = $.extend({
+				offset: 10,
+			}, dj(jq, "affix", 'target'), opt);
+
+			var target = self.target;
+			if (target) target = $(target);
+
+			// namespace
+			self.ns = "affix";
+			self.callback = function() {
+
+				var scrollTop = win.scrollTop();
+
+				if (jq.is('.affix')) {
+
+					if (self.parentTop < jq.parent().offset().top - scrollTop) {
+						jq.addClass("affix-top").removeClass('affix');
+					}
+
+				} else {
+
+					var affixTop = 0,
+						top = jq.offset().top - scrollTop;
+
+					if (target) {
+						if (target.css('position') == 'fixed')
+							affixTop = target.position().top + target.outerHeight();
+					}
+
+					if (self.offset + affixTop >= top) {
+						self.parentTop = jq.parent().offset().top - scrollTop;
+						jq.addClass("affix").removeClass('affix-top').css({
+							top: self.offset + affixTop
+						});
+					}
+				}
+			}
+			onWinScroll.push(self);
+		}
+
+		function onload() {
+			jy.clearTextNode($('[class^="span"]').parent())
+			jy.clearTextNode('nav>dl');
+			jy.accordion('nav>dl>dt');
+			jy.carousel('.carousel');
+			jy.affix('.affix-top');
+			jy.toggle('.toggle');
+			jy.tabs('nav>header>span');
+		}
+
+		if (jy.dont) {
+			//
+		}else{
+			onload();
+		}
+	}
 }(window, window.jQuery);
